3cf8fcc0f459394846d5f9b5c04c7be7c1517482
Sydorenko Olexiy <sydorenkoalex7r@gmail.com>
2026-02-11 19:15:30 +0200

    start 1 exercise

--- tasks_JS/isUnique/isUnuque.js
+++ tasks_JS/isUnique/isUnuque.js
@@ -7,10 +7,10 @@
 // function isUnique(string) {
 // // todo
 // }
-// console.log(isUnique('abcdef')) // -> true
-// console.log(isUnique('1234567')) // -> true
-// console.log(isUnique('abcABC')) // -> true
-// console.log(isUnique('abcadef')) // -> false
+console.log(isUnique('abcdef')) // -> true
+console.log(isUnique('1234567')) // -> true
+console.log(isUnique('abcABC')) // -> true
+console.log(isUnique('abcadef')) // -> false
 
 
 function isUnique(str) {

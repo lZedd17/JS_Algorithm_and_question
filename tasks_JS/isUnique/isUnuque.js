2eac1c36c4ba0b11135faee0018bdd3efb3a42eb
Sydorenko Olexiy <sydorenkoalex7r@gmail.com>
2026-02-12 14:52:03 +0200

    finished 1st exe

--- tasks_JS/isUnique/isUnuque.js
+++ tasks_JS/isUnique/isUnuque.js
@@ -5,30 +5,35 @@
 
 
 // function isUnique(string) {
-// // todo
-// }
 console.log(isUnique('abcdef')) // -> true
 console.log(isUnique('1234567')) // -> true
 console.log(isUnique('abcABC')) // -> true
 console.log(isUnique('abcadef')) // -> false
+/ // todo
+// }
 
 
-function isUnique(str) {
-    for( let i = 0; i < str.length; i++) {
-        if(str.lastIndexOf(str[i]) !== i) {
-            return false
-        }
-    }
-        return true;
-        const chars = new Set()
+function isUnique(string) {
+    // for( let i = 0; i < string.length; i++) {
 
-        for(let i = 0; i < str.length; i++) {
-            const current = str[i];
-            if(chars.has(current)) {
-            return false;
-            }
-            chars.add(current)
-        }
-        return true;
-        return new Set(str).size === str.length;
+    //     const char = string[i];
+
+    //     if(string.lastIndexOf(char) !== i) {
+    //         return false
+    //     }
+    // }
+    //     return true;
+
+        // const set = new Set()
+
+        // for(let i = 0; i < string.length; i++) {
+        //     const char = string[i];
+        //     if(set.has(char)) {
+        //     return false;
+        //     }
+        //     set.add(char)
+        // }
+        // return true;
+
+        return new Set(string).size === string.length;
 }

f54223c9a35473fd9b0106c8a7b88a08fb843a3d
Alex Sydorenko <sydorenkoalex7r@gmail.com>
2026-02-14 10:57:37 +0200

    fineshed 5 exe

--- tasks_JS/rotate.js
+++ tasks_JS/rotate.js
@@ -3,18 +3,20 @@
 // Input: String, String
 // Output: Boolean
 function isStringRotated(source, test) {
-if(source.length !== test.length){
-    return false;
-}
-for(let i=0; i < source.length; i++) {
-    const rotate = 1;
+// if(source.length !== test.length){
+//     return false;
+// }
+// for(let i=0; i < source.length; i++) {
+//     const rotate = source.slice(i, source.length) + source.slice(0, i);
 
-    if( rotate === test) {
-        return true;
-    }
-}
+//     if( rotate === test) {
+//         return true;
+//     }
+// }
 
-return false;
+// return false;
+
+return (source + source).includes(test) && source.length === test.length;
 }
 console.log(isStringRotated('javascript', 'scriptjava')) // -> true
 console.log(isStringRotated('javascript', 'iptjavascr')) // -> true

30df1b7d4ad9306d22304a4cf8c826d589768544
Alex Sydorenko <sydorenkoalex7r@gmail.com>
2026-02-14 11:12:03 +0200

    some changes

--- tasks_JS/rotate.js
+++ tasks_JS/rotate.js
@@ -19,5 +19,5 @@ function isStringRotated(source, test) {
 return (source + source).includes(test) && source.length === test.length;
 }
 console.log(isStringRotated('javascript', 'scriptjava')) // -> true
-console.log(isStringRotated('javascript', 'iptjavascr')) // -> true
+
 console.log(isStringRotated('javascript', 'java')) // -> false
